3bd66d4b0cac815e058fca89f63d06506d569b69
Latifah President <latifahpresident@Latifahs-MBP.lan>
2020-03-26 15:34:18 -0500

    database setup completed

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,26 @@
-// Update with your config settings.
+require('dotenv').config()
 
+localPg = {
+  host: 'localhost',
+  database: process.env.DATABASE,
+  user: 'postgres',
+  password: process.env.DATABASE_PASSWORD,
+  database_url: process.env.DATABASE_URL
+  //password: 'password'
+}
+
+const productionDbConnection = process.env.DATABASE_URL || localPg;
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: localPg, 
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
   },
 
   staging: {
@@ -25,20 +39,19 @@ module.exports = {
     }
   },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  // production: {
+  //   client: 'postgresql',
+  //   connection: process.env.DATABASE_URL,
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: './database/migrations'
+  //   },
+  //   seeds: {
+  //     directory: './database/seeds'
+  //   },
+  // }
 
 };
