5ec4fb55b61e49fec25882868f40102af57bcbca
Latifah President <latifahpresident@Latifahs-MBP.lan>
2020-04-03 16:06:42 -0500

    fixed headers for stripe, cannot be a wild card

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(helmet());
 app.use(compression());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
     res.header("Access-Control-Allow-Origin: http://localhost:3000")
