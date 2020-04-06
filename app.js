97a0d28c82a09eef99068726c4354043eae0a108
Latifah <latifah.pres@gmail.com>
2020-04-06 18:44:16 -0500

    changing access control header to access pocketshop backend link

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
     // res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-    res.header("Access-Control-Allow-Origin", "https://pocket-shop.herokuapp.com")
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
 
     next();
 });
